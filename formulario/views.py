ab6af1f69468cb4cfd94df885c646f0dfe6194d7
unknown <paolo.minaya.c@gmail.com>
2020-02-16 11:11:40 -0500

    cambios que no sirvieron

--- formulario/views.py
+++ formulario/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404, redirect
 from .models import Cliente, Mascota, Paseador
 from .forms import ClienteForm, MascotaFormm, PaseadorForm
+from django.contrib import messages
+from django.contrib.auth import authenticate
+
 
 
 def Register_Cliente(request):
@@ -8,18 +11,23 @@ def Register_Cliente(request):
         form=ClienteForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
+        return redirect('register_cliente')
     else:
         form=ClienteForm()
     return render(request,'formulario.html',{'form':form})
+
 
 def Register_mascota(request):
     if request.method=="POST":
         form=MascotaFormm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
+            form.cleaned_data
+            messages.success(request, 'se cargo la información')
     else:
         form=MascotaFormm()
     return render(request,'formulario_mascota.html',{'form':form})
+
 
 def Register_paseador(request):
     if request.method=="POST":
